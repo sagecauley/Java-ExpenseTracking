8267c600708c8e75923404bb3a24c63ed4147fcc
running5k <running5k312@gmail.com>
2025-01-05 18:02:59 -0600

    Added a text box to the UI

--- src/HomeView.java
+++ src/HomeView.java
@@ -1,5 +1,6 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.geometry.Pos;
 
@@ -9,8 +10,12 @@ public class HomeView {
     public HomeView() {
         root = new StackPane();  // Create the layout
         Label label = new Label("Hello World, JavaFX! On HomeView");
+        TextField nameTextField = new TextField();
+        nameTextField.setPromptText("Enter name here");
+        nameTextField.setAlignment(Pos.BASELINE_LEFT);
         label.setAlignment(Pos.CENTER);  // Center the label text
         root.getChildren().add(label);  // Add the label to the root
+        root.getChildren().add(nameTextField);
         
     }
 
